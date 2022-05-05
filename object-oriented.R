21a7f89c029f049a9f0afc0d77147d782b00bf6a
Rod Morgado <rodrigo@zooft.company>
2022-05-05 12:54:01 -0700

    Private fields can be accessed using private$

--- object-oriented.R
+++ object-oriented.R
@@ -169,3 +169,26 @@ a_microwave_oven <- microwave_oven_factory$new()
 
 # Call cook method for 1 second
 a_microwave_oven$cook(1)
+
+# Add a close_door() method
+microwave_oven_factory <- R6Class(
+  "MicrowaveOven",
+  private = list(
+    power_rating_watts = 800,
+    door_is_open = FALSE
+  ),
+  public = list(
+    cook = function(time_seconds) {
+      Sys.sleep(time_seconds)
+      print("Your food is cooked!")
+    },
+    open_door = function() {
+      private$door_is_open <- TRUE
+    },
+    close_door = function() {
+      private$door_is_open <- FALSE
+    }
+    
+    
+  )
+)
