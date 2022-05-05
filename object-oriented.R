b994db12f40e9555f4a5567b5384f29cb62b296f
Rod Morgado <rodrigo@zooft.company>
2022-05-05 13:16:39 -0700

    Delightful coding! Active bindings let users set data fields in a controlled way.

--- object-oriented.R
+++ object-oriented.R
@@ -248,3 +248,43 @@ a_microwave_oven <- microwave_oven_factory$new()
 
 # Get the power rating
 a_microwave_oven$power_rating_watts
+
+# Add a binding for power rating
+microwave_oven_factory <- R6Class(
+  "MicrowaveOven",
+  private = list(
+    ..power_rating_watts = 800,
+    ..power_level_watts = 800
+  ),
+  # Add active list containing an active binding
+  active = list(
+    power_level_watts = function(value) {
+      if(missing(value)) {
+        # Return the private value
+        private$..power_level_watts
+      } else {
+        # Assert that value is a number
+        assert_is_a_number(value)
+        # Assert that value is in a closed range from 0 to power rating
+        assert_all_are_in_closed_range(value, lower = 0, upper = private$..power_rating_watts)
+        # Set the private power level to value
+        private$..power_level_watts <- value
+      }
+    }
+  )
+)
+
+# Make a microwave 
+a_microwave_oven <- microwave_oven_factory$new()
+
+# Get the power level
+a_microwave_oven$power_level_watts
+
+# Try to set the power level to "400"
+a_microwave_oven$power_level_watts <- "400"
+
+# Try to set the power level to 1600 watts
+a_microwave_oven$power_level_watts <- 1600
+
+# Set the power level to 400 watts
+a_microwave_oven$power_level_watts <- 400
