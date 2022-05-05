068d77ceae0a8c878865ba56c8ccbec2226b427c
Rod Morgado <rodrigo@zooft.company>
2022-05-04 17:05:04 -0700

    Functions can behave differently depending upon the type of input.

---
+++ object-oriented.R
@@ -0,0 +1,18 @@
+# Create these variables
+a_numeric_vector <- rlnorm(50)
+a_factor <- factor(
+  sample(c(LETTERS[1:5], NA), 50, replace = TRUE)
+)
+a_data_frame <- data.frame(
+  n = a_numeric_vector,
+  f = a_factor
+)
+a_linear_model <- lm(dist ~ speed, cars)
+
+# Call summary() on the numeric vector
+summary(a_numeric_vector)
+
+# Do the same for the other three objects
+summary(a_factor)
+summary(a_data_frame)
+summary(a_linear_model)
