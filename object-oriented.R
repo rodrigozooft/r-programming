85fc7394dce8dada6fd45046f2d1eac92a5b2707
Rod Morgado <rodrigo@zooft.company>
2022-05-04 17:16:24 -0700

    class(), typeof(), mode(), and storage.mode() can take different values for the same variable type.

--- object-oriented.R
+++ object-oriented.R
@@ -16,3 +16,23 @@ summary(a_numeric_vector)
 summary(a_factor)
 summary(a_data_frame)
 summary(a_linear_model)
+
+# Look at the definition of type_info()
+type_info
+
+# Create list of example variables
+some_vars <- list(
+  an_integer_vector = rpois(24, lambda = 5),
+  a_numeric_vector = rbeta(24, shape1 = 1, shape2 = 1),
+  an_integer_array = array(rbinom(24, size = 8, prob = 0.5), dim = c(2, 3, 4)),
+  a_numeric_array = array(rweibull(24, shape = 1, scale = 1), dim = c(2, 3, 4)),
+  a_data_frame = data.frame(int = rgeom(24, prob = 0.5), num = runif(24)),
+  a_factor = factor(month.abb),
+  a_formula = y ~ x,
+  a_closure_function = mean,
+  a_builtin_function = length,
+  a_special_function = `if`
+)
+
+# Loop over some_vars calling type_info() on each element to explore them
+lapply(some_vars, type_info)
