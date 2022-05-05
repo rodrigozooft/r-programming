79f5108651cc3989edc14fa0ea3c3891db125d26
Rod Morgado <rodrigo@zooft.company>
2022-05-04 21:02:24 -0700

    Variables can have many classes

--- object-oriented.R
+++ object-oriented.R
@@ -86,3 +86,20 @@ exists("length.hairstylist")
 
 # What is the length of hair?
 length(hair)
+
+# View the kitty
+kitty
+
+# Assign classes
+class(kitty) <- c("cat", "mammal", "character")
+
+# Does kitty inherit from cat/mammal/character?
+inherits(kitty, "cat")
+inherits(kitty, "mammal")
+inherits(kitty, "character")
+
+# Is kitty a character vector?
+is.character(kitty)
+
+# Does kitty inherit from dog?
+inherits(kitty, "dog")
