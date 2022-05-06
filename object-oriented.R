e7675115849e0e03bc8711f448ea1b4b8ed53b73
Rod Morgado <rodrigo@zooft.company>
2022-05-06 15:05:25 -0700

    Independent copies of R6 objects can be made using the clone() method.

--- object-oriented.R
+++ object-oriented.R
@@ -466,3 +466,21 @@ a_microwave_oven$safety_warning <- "Warning. If the food is too hot you may scal
   
 # Verify that the warning has change for another_microwave
 another_microwave_oven$safety_warning
+
+# Create a microwave oven
+a_microwave_oven <- microwave_oven_factory$new()
+
+# Copy a_microwave_oven using <-
+assigned_microwave_oven <- a_microwave_oven
+  
+# Copy a_microwave_oven using clone()
+cloned_microwave_oven <- a_microwave_oven$clone()
+  
+# Change a_microwave_oven's power level  
+a_microwave_oven$power_level_watts <- 400
+  
+# Check a_microwave_oven & assigned_microwave_oven same 
+identical(a_microwave_oven$power_level_watts, assigned_microwave_oven$power_level_watts)
+
+# Check a_microwave_oven & cloned_microwave_oven different 
+identical(a_microwave_oven, cloned_microwave_oven)  
