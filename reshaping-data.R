da0eca0d72f0b3629e8cd5c6a372bd2a4e490b79
Rod Morgado <rodrigo@zooft.company>
2022-05-07 14:58:14 -0700

    It looks like 1961 was the year humas replaced dogs in space. Adding zero observations allowed you to create a corret and complete plot.

--- reshaping-data.R
+++ reshaping-data.R
@@ -198,3 +198,18 @@ codon_df <- expand_grid(
 codon_df %>% 
   # Unite these three columns into a "codon" column
   unite(col = "codon", sep = "")
+
+# Create a tibble with all combinations of years and species
+full_df <- expand_grid(
+  year = 1951:1970, 
+  species = c("Human", "Dog")
+)
+
+space_df %>% 
+  # Join with full_df so that missing values are introduced
+  right_join(full_df, by = c("year", "species")) %>% 
+  # Overwrite NA values for n_in_space with 0L
+  replace_na(list(n_in_space = 0L)) %>% 
+  # Create a line plot with n_in_space over year, color by species
+  ggplot(aes(x = year, y = n_in_space, color = species)) +
+  geom_line()
