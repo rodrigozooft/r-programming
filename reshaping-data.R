aff909b9e0acfb10faab4d6131e93d993689db7b
Rod Morgado <rodrigo@zooft.company>
2022-05-07 12:27:06 -0700

    Notice how the 'name' and 'gender parts of the old column headers were reused in two new columns. The second part of the old headers is found as a variable in the dog_id column. Since, most launchess had two dogs, the number of rows almost doubled.'

--- reshaping-data.R
+++ reshaping-data.R
@@ -127,3 +127,14 @@ stock_df %>%
   ggplot(aes(y = price, x = week, color = company)) +
   geom_line() +
   facet_grid(. ~ year)
+
+space_dogs_df %>% 
+  pivot_longer(
+    # Add the columns to pivot
+    name_1:gender_2,
+    names_sep = "_",
+    # Complete the names_to argument to re-use the first part of the column headers
+    names_to = c('.value',  "dog_id"),
+    # Make sure NA values are dropped
+    values_drop_na = TRUE
+  )
