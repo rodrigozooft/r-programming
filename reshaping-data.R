8e4f68e8e590e17023ffcdf25c65acadec40b9a0
Rod Morgado <rodrigo@zooft.company>
2022-05-06 17:53:10 -0700

    Being able to unite columns is a good skill to have but practice you'll be separating them more often while tidying data.

--- reshaping-data.R
+++ reshaping-data.R
@@ -1,3 +1,7 @@
 netflix_df %>% 
   # Split the duration column into value and unit columns
   separate(duration, into = c('value', 'unit'), sep = " ", convert = TRUE)
+
+phone_nr_df %>%
+  # Unite the country_code and national_number columns
+  unite("international_number", country_code, national_number, sep = " ")
