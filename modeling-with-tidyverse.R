3934c227a6ca81d762658d271a9600d6fafe645c
Rod Morgado <rodrigo@zooft.company>
2022-05-09 10:45:04 -0700

    As suggested in the previous histogram for age, the center of the distribution as qunatified by the mean and median is around 48 years old

--- modeling-with-tidyverse.R
+++ modeling-with-tidyverse.R
@@ -6,3 +6,13 @@ library(ggplot2)
 ggplot(evals, aes(x = age)) +
   geom_histogram(binwidth = 5) +
   labs(x = "age", y = "count")
+
+# Load packages
+library(moderndive)
+library(dplyr)
+
+# Compute summary stats
+evals %>%
+  summarize(mean_age = mean(age),
+            median_age = median(age),
+            sd_age = sd(age))
