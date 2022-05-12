b0f00322941480379d5cf1d59b625590a7b15ea6
Rod Morgado <rodrigo@zooft.company>
2022-05-12 16:02:29 -0700

    There are more handy functions - summarize_at() and mutate_at(). These let you select columns by name-based select helpers.

--- qualitative-data-tidyverse.R
+++ qualitative-data-tidyverse.R
@@ -3,3 +3,13 @@ print(multiple_choice_responses)
 
 # Check if CurrentJobTitleSelect is a factor
 is.factor(multiple_choice_responses$CurrentJobTitleSelect)
+
+# Change all the character columns to factors
+responses_as_factors <- multiple_choice_responses %>%
+    mutate_if(is.character, as.factor)
+
+number_of_levels <- responses_as_factors %>%
+	# apply the function nlevels to each column
+    summarise_all(nlevels) %>%
+    # change the dataset from wide to long
+    gather(variable, num_levels)
